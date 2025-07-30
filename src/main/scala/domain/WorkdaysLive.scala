e146fde3191e4122b37a69ae772876e9ffe05ea8
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-07-29 21:17:51 -0300

    parse date

--- src/main/scala/domain/WorkdaysLive.scala
+++ src/main/scala/domain/WorkdaysLive.scala
@@ -3,9 +3,19 @@ package domain
 
 import zio.{ULayer, ZIO, ZLayer}
 
+import java.time.LocalDate
+
 case class WorkdaysLive() extends Workdays {
 
-  override def isWorkday(date: String): ZIO[Workdays, Nothing, BooleanResponse] = ZIO.succeed(BooleanResponse(true))
+  override def isWorkday(date: String): ZIO[Workdays, Nothing, BooleanResponse] = {
+    for {
+      _ <- parseDate(date)
+    } yield BooleanResponse(true)
+  }
+
+  private def parseDate(date: String): ZIO[Workdays, Nothing, LocalDate] =
+    ZIO.attempt(LocalDate.parse(date))
+      .catchAll(e => ZIO.log(s"error: ${e}").as(LocalDate.now()))
 }
 
 object WorkdaysLive {
