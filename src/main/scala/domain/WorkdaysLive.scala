4800a1aebf42fdf64ec28bb058dddf1b832fed02
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2025-08-07 18:05:38 -0300

    initial holidays calc logic

--- src/main/scala/domain/WorkdaysLive.scala
+++ src/main/scala/domain/WorkdaysLive.scala
@@ -7,15 +7,47 @@ import java.time.LocalDate
 
 case class WorkdaysLive() extends Workdays {
 
+  /*
+  *Confraternização Universal	- 1º de janeiro	- Início do ano civil.
+  Tiradentes	- 21 de abril	- Em homenagem ao mártir da Inconfidência Mineira Joaquim José da Silva Xavier, o Tiradentes.
+  Dia do Trabalhador	- 1º de maio	- Homenagem a todos os trabalhadores.
+  Dia da Pátria (Independência do Brasil) (Data Magna Nacional)	- 7 de setembro	- Proclamação da Independência em relação a Portugal.
+  Nossa Senhora Aparecida	- 12 de outubro[5] - Padroeira do Brasil.
+  Finados	- 2 de novembro	- Dia em memória aos mortos.
+  Proclamação da República	- 15 de novembro	- Transformação do Império em República.
+  Dia Nacional de Zumbi e da Consciência Negra	- 20 de novembro -	Data da Morte de Zumbi dos Palmares.[6]
+  Natal	- 25 de dezembro	- Celebração do nascimento de Jesus.
+  * */
+  private val currentYear = LocalDate.now().getYear
+  private val diaPrimeiro = LocalDate.of(currentYear, 1, 1)
+  private val tiradentes = LocalDate.of(currentYear, 4, 21)
+  private val independencia = LocalDate.of(currentYear, 9, 7)
+  private val nossaSenhora = LocalDate.of(currentYear, 10, 12)
+  private val finados = LocalDate.of(currentYear, 11, 2)
+  private val proclamacaoRepublica = LocalDate.of(currentYear, 12, 15)
+  private val conscienciaNegra = LocalDate.of(currentYear, 11, 20)
+  private val natal = LocalDate.of(currentYear, 12, 25)
+
+  private val holidays = List(diaPrimeiro, tiradentes, independencia, nossaSenhora, finados, proclamacaoRepublica, conscienciaNegra, natal)
+
   override def isWorkday(date: String): ZIO[Workdays, Nothing, BooleanResponse] = {
     for {
-      _ <- parseDate(date)
-    } yield BooleanResponse(true)
+      date <- parseDate(date)
+      isHoliday <- ZIO.succeed(holidays.contains(date))
+    } yield BooleanResponse(!isHoliday)
   }
 
-  private def parseDate(date: String): ZIO[Workdays, Nothing, LocalDate] =
-    ZIO.attempt(LocalDate.parse(date))
-      .catchAll(e => ZIO.log(s"error: ${e}").as(LocalDate.now()))
+  private def parseDate(date: String): ZIO[Workdays, Nothing, LocalDate] = {
+    for {
+      parts <- ZIO.succeed(date.split("-"))
+      finalDate <- ZIO.attempt{
+        val day = parts(0).toInt
+        val month = parts(1).toInt
+        val year = parts(2).toInt
+        LocalDate.of(year, month, day)
+      }.catchAll(e => ZIO.log(s"error: ${e}").as(LocalDate.now()))
+    } yield finalDate
+  }
 }
 
 object WorkdaysLive {
